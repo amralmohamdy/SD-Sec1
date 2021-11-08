2e16a3d4429a8b9530ba5a131178c116e3130297
amralmohamdy <42351956+amralmohamdy@users.noreply.github.com>
2021-11-08 20:16:32 +0200

    Add files via upload

---
+++ Assignment3/Mammal.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkginterface;
+
+/**
+ *
+ * @author Dell
+ */
+public class Mammal implements animal  {
+
+    
+    public void eat() {}    
+    public void travel() {}
+    public int NoOfLegs(){return 0;}
+    public String FavFood(){return "leaves, stems, roots and nuts";}
+   
+    
+}
